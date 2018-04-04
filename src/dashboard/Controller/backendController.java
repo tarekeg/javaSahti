016f10575d737e9bb6d992573c6229720656b77e
tarek <tarek@Tarek-PC>
2018-04-04 18:17:12 +0100

    First commit

--- src/dashboard/Controller/backendController.java
+++ src/dashboard/Controller/backendController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dashboard;
+package dashboard.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author tarek
  */
-public class DashController implements Initializable {
+public class backendController implements Initializable {
 
     /**
      * Initializes the controller class.
